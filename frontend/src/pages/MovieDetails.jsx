c8dfdcb74d882c1ba45a0a4e60703402f545accb
Pramodsnz01 <pramodsnz01@gmail.com>
2026-02-18 17:48:49 +0545

    Completed MovieDetails Page

--- frontend/src/pages/MovieDetails.jsx
+++ frontend/src/pages/MovieDetails.jsx
@@ -1,21 +1,26 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom'
+import { useNavigate, useParams } from 'react-router-dom'
 import { dummyDateTimeData, dummyShowsData } from '../assets/assets'
 import BlurCircle from '../components/BlurCircle'
 import { Heart, PlayCircleIcon, StarIcon } from 'lucide-react'
 import timeFormat from '../lib/timeFormat'
 import DateSelect from '../components/DateSelect'
+import MovieCard from '../components/MovieCard'
+import Loading from '../components/Loading'
 
 const MovieDetails = () => {
+  const navigate = useNavigate() ;
   const { id } = useParams() // Get the movie ID from the URL parameters
   const [show, setShow] = useState(null) // State to hold the movie details
 
   const getShow = async () => {
     const show = dummyShowsData.find(show => show._id === id)
-    setShow({
-      movie: show,
-      dateTime: dummyDateTimeData
-    })
+    if(show){
+      setShow({
+        movie: show,
+        dateTime: dummyDateTimeData
+      })
+    }
   }
   useEffect(() => {
     getShow()
@@ -70,8 +75,18 @@ const MovieDetails = () => {
 
       <DateSelect dateTime={show.dateTime} id={id} />
 
+      <p className="text-lg font-medium mt-20 mb-8">You May Also Like</p>
+      <div className="flex flex-wrap max-sm:justify-center gap-8">
+        {dummyShowsData.slice(0, 4).map((movie, index)=> (
+          <MovieCard key={index} movie={movie} />
+        ))}
+      </div>
+      <div className="flex justify-center mt-20">
+        <button onClick={()=> {navigate('/movies'); scrollTo(0,0)}} className="px-10 py-3 text-sm bg-primary hover:bg-primary-dull transition rounded-md font-medium cursor-pointer">Show more</button>
+      </div>
+
     </div>
-  ) : <div className="">Loading...</div>
+  ) : <Loading />
 }
 
 export default MovieDetails
