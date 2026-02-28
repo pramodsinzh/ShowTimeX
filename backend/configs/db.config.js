0b244a42af8b936bed36ab9b18a6e4a7fe54fba8
Pramodsnz01 <pramodsnz01@gmail.com>
2026-02-28 17:26:01 +0545

    Backend folder setup and connected to mongo db

---
+++ backend/configs/db.config.js
@@ -0,0 +1,11 @@
+import mongoose from "mongoose";
+
+const connectDB = async ()=> {
+    try {
+        mongoose.connection.on('connected', ()=> console.log("Connected to MongoDB"))
+        await mongoose.connect(`${process.env.MONGODB_URI}/showTimeX`)
+    } catch (error) {
+        console.log(error.message) 
+    }
+}
+export default connectDB
