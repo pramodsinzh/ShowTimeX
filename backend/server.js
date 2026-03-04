b6384e3d9e2b9643d97c767eb414fc842eab0c2b
Pramodsnz01 <pramodsnz01@gmail.com>
2026-03-04 16:39:12 +0545

    created user model and connected innjest with clerk and then with backend

--- backend/server.js
+++ backend/server.js
@@ -2,6 +2,11 @@ import express from 'express'
 import cors from 'cors'
 import 'dotenv/config'
 import connectDB from './configs/db.config.js';
+import { clerkMiddleware } from '@clerk/express'
+import { serve } from 'inngest/express'
+import { inngest, functions } from './inngest/index.js';
+
+
 
 const app = express()
 const port = 3000;
@@ -11,8 +16,12 @@ await connectDB()
 //middleware
 app.use(express.json())
 app.use(cors())
+app.use(clerkMiddleware())
+
+
 
 //Api routes
-app.get('/', (req, res)=> res.send("Server is live!"))
+app.get('/', (req, res) => res.send("Server is live!"))
+app.use('/api/inngest', serve({ client: inngest, functions }))
 
-app.listen(port, ()=> console.log(`Server listening at http://localhost:${port}`))
+app.listen(port, () => console.log(`Server listening at http://localhost:${port}`))
