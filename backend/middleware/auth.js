c71025efb92f87eae5b544f3fcc2fb2db1840d02
Pramodsnz01 <pramodsnz01@gmail.com>
2026-03-06 17:46:40 +0545

    Added auth middleware to the admin routes

---
+++ backend/middleware/auth.js
@@ -0,0 +1,14 @@
+import { clerkClient } from "@clerk/express";
+
+export const protectAdmin = async (req, res, next) => {
+    try {
+        const { userId } = req.auth();
+        const user = await clerkClient.users.getUsers(userId)
+        if (user.privateMetadata.role !== 'admin') {
+            return res.json({ success: true, message: "not auhorized" })
+        }
+        next()
+    } catch (error) {
+        return res.json({ success: false, error: error.message })
+    }
+}
