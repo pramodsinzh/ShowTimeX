c44c69d8379d27be5d3f99f92bdaeb29b40062dd
Pramodsnz01 <pramodsnz01@gmail.com>
2026-03-08 16:18:52 +0545

    Created router for admin

--- backend/server.js
+++ backend/server.js
@@ -7,6 +7,7 @@ import { serve } from 'inngest/express'
 import { inngest, functions } from './inngest/index.js';
 import showRouter from './routes/showRoutes.js';
 import bookingRouter from './routes/bookingRoutes.js';
+import adminRouter from './routes/adminRoutes.js';
 
 
 
@@ -26,9 +27,13 @@ app.use(clerkMiddleware())
 app.get('/', (req, res) => res.send("Server is live!"))
 app.use('/api/inngest', serve({ client: inngest, functions }))
 
-//Movies routes
+//Movies route
 app.use('/api/show', showRouter)
-//Booking routes
+
+//Booking route
 app.use('/api/booking', bookingRouter)
+
+//Admin Route
+app.use('/api/admin', adminRouter)
 
 app.listen(port, () => console.log(`Server listening at http://localhost:${port}`))
