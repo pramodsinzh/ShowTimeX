1bc8563fd37f94ddd156cf6ba6fdb45b9952c754
Pramodsnz01 <pramodsnz01@gmail.com>
2026-03-10 17:02:29 +0545

    Completed Add Show feature for admin

--- frontend/src/context/AppContext.jsx
+++ frontend/src/context/AppContext.jsx
@@ -14,6 +14,8 @@ export const AppProvider = ({ children }) => {
     const [shows, setShows] = useState([])
     const [favoriteMovies, setFavoriteMovies] = useState([])
 
+    const image_base_url = import.meta.env.VITE_TMDB_IMAGE_BASE_URL
+
     const { user } = useUser()
     const { getToken } = useAuth()
     const location = useLocation()
@@ -71,7 +73,7 @@ export const AppProvider = ({ children }) => {
         }
     }, [user])
 
-    const value = { axios, fetchIsAdmin, user, getToken, navigate, isAdmin, shows, favoriteMovies, fetchFavoriteMovies }
+    const value = { axios, fetchIsAdmin, user, getToken, navigate, isAdmin, shows, favoriteMovies, fetchFavoriteMovies, image_base_url }
     return (
         <AppContext.Provider value={value}>
             {children}
