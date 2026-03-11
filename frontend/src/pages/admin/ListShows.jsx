6b84f5e54363a096cfbadfa28dcb8dd1f3ea2cb0
Pramodsnz01 <pramodsnz01@gmail.com>
2026-03-11 17:10:38 +0545

    List shows feature for admin is completed

--- frontend/src/pages/admin/ListShows.jsx
+++ frontend/src/pages/admin/ListShows.jsx
@@ -3,34 +3,31 @@ import { dummyShowsData } from '../../assets/assets'
 import Loading from '../../components/Loading'
 import Title from '../../components/admin/Title'
 import { dateFormat } from '../../lib/dateFormat'
+import { useAppContext } from '../../context/AppContext'
 
 const ListShows = () => {
   const currency = import.meta.env.VITE_CURRENCY
+  const { axios, getToken, user } = useAppContext()
 
   const [shows, setShows] = useState([])
   const [loading, setLoading] = useState(true)
 
   const getAllShows = async () => {
     try {
-      setShows([{
-        movie: dummyShowsData[0],
-        showDateTime: '2025-07-24T01:00:00.000Z',
-        showPrice: 59,
-        occupiedSeats: {
-          A1: 'user_1',
-          B1: 'user_2',
-          C1: 'user_3',
-        }
-
-      }])
+      const { data } = await axios.get('/api/admin/all-shows', {
+        headers: { Authorization: `Bearer ${await getToken()}` }
+      })
+      setShows(data.shows)
       setLoading(false)
     } catch (error) {
-        console.log(error)
+      console.log(error)
     }
   }
-  useEffect(()=> {
-    getAllShows();
-  }, [])
+  useEffect(() => {
+    if (user) {
+      getAllShows();
+    }
+  }, [user])
 
   return !loading ? (
     <>
@@ -46,7 +43,7 @@ const ListShows = () => {
             </tr>
           </thead>
           <tbody className='text-sm font-light'>
-            {shows.map((show, index)=> (
+            {shows.map((show, index) => (
               <tr className="border-b border-primary/10 bg-primary/5 even:bg-primary/10" key={index}>
                 <td className="p-2 min-w-45 pl-5">{show.movie.title}</td>
                 <td className="p-2 ">{dateFormat(show.showDateTime)}</td>
@@ -57,7 +54,7 @@ const ListShows = () => {
           </tbody>
         </table>
       </div>
-    </> 
+    </>
   ) : <Loading />
 }
 
