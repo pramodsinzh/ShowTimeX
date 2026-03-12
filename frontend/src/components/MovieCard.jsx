0f7a06e9472137e16fc23620c64e6cb80c6d0048
Pramodsnz01 <pramodsnz01@gmail.com>
2026-03-12 08:51:17 +0545

    Updated featured section

--- frontend/src/components/MovieCard.jsx
+++ frontend/src/components/MovieCard.jsx
@@ -1,12 +1,14 @@
 import { StarIcon } from "lucide-react";
 import { useNavigate } from "react-router-dom"
 import timeFormat from "../lib/timeFormat";
+import { useAppContext } from "../context/AppContext";
 
 const MovieCard = ({ movie }) => {
     const navigate = useNavigate()
+    const {image_base_url} = useAppContext()
     return (
         <div className='flex flex-col justify-between p-3 bg-gray-800 rounded-xl hover:translate-y-1 transition duration-300 w-66'>
-            <img onClick={() => { navigate(`/movies/${movie._id}`); scrollTo(0, 0) }} src={movie.backdrop_path} className="rounded-lg h-52 w-full object-cover object-bottom-right cursor-pointer" alt="" />
+            <img onClick={() => { navigate(`/movies/${movie._id}`); scrollTo(0, 0) }} src={image_base_url + movie.backdrop_path} className="rounded-lg h-52 w-full object-cover object-bottom-right cursor-pointer" alt="" />
 
             <p className="font-semibold mt-2 truncate">{movie.title}</p>
 
