25bbcca9b0067c6ca79c6abb02a1db26dd55924e
Pramodsnz01 <pramodsnz01@gmail.com>
2026-03-14 17:55:30 +0545

    loader fixes

--- frontend/src/components/Loading.jsx
+++ frontend/src/components/Loading.jsx
@@ -8,7 +8,7 @@ const Loading = () => {
   useEffect(()=>{
     if(nextUrl){
       setTimeout(() => {
-        navigate('/', nextUrl)
+        navigate(`/${nextUrl}`)
       }, 8000);
     }
   }, [])
